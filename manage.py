89ee253bbad271767388cb919a2447f93811064e
AP_Anton_Maker <andore.pro@gmail.com>
2026-04-16 09:24:08 +0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,6 +3,21 @@ import os
 import sys
 
 def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Не удалось импортировать Django. Убедитесь, что оно установлено."
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()#!/usr/bin/env python
+import os
+import sys
+
+def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'laser_crm.settings')
     try:
         from django.core.management import execute_from_command_line
